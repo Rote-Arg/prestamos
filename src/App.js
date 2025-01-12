77b1712e9f347bbd657cacf70f9394bed0d3d531
Rote-Arg <elrote@gmail.com>
2025-01-12 00:47:00 -0300

    Fixed the changes for update react and node

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
                   total={total}
                   plazo={plazo}
                   cantidad={cantidad}
-                 />
+                />
   }
 
   return (
